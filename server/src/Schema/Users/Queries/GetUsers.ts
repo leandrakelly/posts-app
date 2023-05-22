24415a3744b31b96437f0eb93017315dafde197c
Keri ♡ <leandrakell@gmail.com>
2023-05-22 14:40:56 -0300

    feat(server): add get users query

--- server/src/Schema/Users/Queries/GetUsers.ts
+++ server/src/Schema/Users/Queries/GetUsers.ts
@@ -1,11 +1,15 @@
 import { GraphQLList } from 'graphql';
 import { UserType } from '../TypeDefs/User';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
 const prisma = new PrismaClient();
 export const GET_USERS = {
   type: new GraphQLList(UserType),
-  resolve(): any {
-    return prisma.user.findMany();
+  resolve(): Promise<User[]> {
+    return prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    });
   },
 };
