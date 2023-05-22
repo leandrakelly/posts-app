ca2c1f6ff08114b104c58755bf13b19b71004d5c
Keri ♡ <leandrakell@gmail.com>
2023-05-22 14:34:18 -0300

    feat: add login page

---
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { Login } from '../../components/auth/Login';
+import '../../components/auth/index.css';
+
+export const LoginPage = () => {
+  return (
+    <div className="auth-page">
+      <Login />
+    </div>
+  );
+};
