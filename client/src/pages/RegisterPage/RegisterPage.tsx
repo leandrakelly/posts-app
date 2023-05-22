22bd6ecd0102624305e19bb2380465c93696c3c0
Keri ♡ <leandrakell@gmail.com>
2023-05-22 14:34:31 -0300

    feat: add register page

---
+++ client/src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Register from '../../components/auth/Register';
+import '../../components/auth/index.css';
+
+export const RegisterPage = () => {
+  return (
+    <div className="auth-page">
+      <Register />
+    </div>
+  );
+};
