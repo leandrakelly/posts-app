a1f9dd560c924e0bf6c28c1a42a6dfa8f3c53b2c
Keri ♡ <leandrakell@gmail.com>
2023-05-22 14:38:58 -0300

    feat(server): add user typedefs

--- server/src/Schema/Users/TypeDefs/User.ts
+++ server/src/Schema/Users/TypeDefs/User.ts
@@ -1,20 +1,28 @@
-import {
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLList,
-} from 'graphql';
-import { PostType } from '../../Post';
+import { GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
+import { PostType } from '../../Posts/TypeDefs/Post';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
-    id: { type: GraphQLID },
+    id: { type: GraphQLString },
     email: { type: GraphQLString },
     password: { type: GraphQLString },
     name: { type: GraphQLString },
     createdAt: { type: GraphQLString },
     updatedAt: { type: GraphQLString },
-    posts: { type: new GraphQLList(PostType) },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async (parent) => {
+        const userPosts = await prisma.post.findMany({
+          where: {
+            authorId: parent.id,
+          },
+        });
+        return userPosts;
+      },
+    },
   }),
 });
