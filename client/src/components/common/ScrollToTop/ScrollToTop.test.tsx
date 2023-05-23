4a08fbcca74636181e829b5e89b344d2db3fce42
Keri ♡ <leandrakell@gmail.com>
2023-05-23 05:46:29 -0300

    test: add scroll to top component test

---
+++ client/src/components/common/ScrollToTop/ScrollToTop.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import ScrollToTopButton from './ScrollToTop';
+
+describe('ScrollToTopButton', () => {
+  test('renders the button', () => {
+    render(<ScrollToTopButton />);
+
+    const buttonElement = screen.getByRole('button');
+
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('scroll-to-top');
+    expect(buttonElement).toHaveClass('hidden');
+  });
+
+  test('scrolls to top when clicked', () => {
+    window.scrollTo = jest.fn();
+
+    render(<ScrollToTopButton />);
+
+    const buttonElement = screen.getByRole('button');
+
+    fireEvent.click(buttonElement);
+
+    expect(window.scrollTo).toHaveBeenCalledWith({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
