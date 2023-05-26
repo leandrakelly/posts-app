d62dc20212ee46cafa76b000c0cee658713681a4
Keri ♡ <leandrakell@gmail.com>
2023-05-26 09:21:20 -0300

    chore: lint

--- server/src/Middleware/auth-validator.ts
+++ server/src/Middleware/auth-validator.ts
@@ -1,39 +1,40 @@
 import { FieldNode, parse } from 'graphql';
 import { JwtPayload, verify } from 'jsonwebtoken';
-import { CustomError } from "../Errors/CustomError";
+import { CustomError } from '../Errors/CustomError';
 import { PrismaClient } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 
-const UN_PROTECTED_ROUTES = [
-  'login',
-  'createUser'
-];
+const UN_PROTECTED_ROUTES = ['login', 'createUser'];
 
-const getToken = (authorization: string): string => { 
+const getToken = (authorization: string): string => {
   if (!authorization.startsWith('Bearer'))
     throw new CustomError('Unauthorized: Invalid token', '401');
-  const [,token] = authorization.split(' ');
-  if (!token)
-    throw new CustomError('Unauthorized: Invalid token', '401');
+  const [, token] = authorization.split(' ');
+  if (!token) throw new CustomError('Unauthorized: Invalid token', '401');
   return token;
-}
+};
 
-const isProtectedRoute = (query: string): boolean => { 
-  const { definitions: [operation] } = parse(query);
-  const route = operation.kind === 'OperationDefinition' ? (operation.selectionSet.selections[0] as FieldNode).name.value : '';
+const isProtectedRoute = (query: string): boolean => {
+  const {
+    definitions: [operation],
+  } = parse(query);
+  const route =
+    operation.kind === 'OperationDefinition'
+      ? (operation.selectionSet.selections[0] as FieldNode).name.value
+      : '';
   const isProtectedRoute = !UN_PROTECTED_ROUTES?.includes(route);
   return isProtectedRoute;
-}
+};
 
-const getUserIdFromToken = (authorization: string | undefined): string => { 
+const getUserIdFromToken = (authorization: string | undefined): string => {
   if (!authorization)
     throw new CustomError('Unauthorized: Invalid token', '401');
 
   const token = getToken(authorization);
   const secret = process.env.JWT_SECRET || '';
-  const decode = () => { 
+  const decode = () => {
     try {
       return verify(token, secret) as JwtPayload;
     } catch (error) {
@@ -43,15 +44,18 @@ const getUserIdFromToken = (authorization: string | undefined): string => {
   const decoded = decode();
 
   return decoded.userId;
-}
+};
 
-const validateUser = async (id: string) => { 
+const validateUser = async (id: string) => {
   const user = await prisma.user.findUnique({ where: { id } });
-  if (!user) 
-    throw new CustomError('Unauthorized: Invalid token', '401');
-}
+  if (!user) throw new CustomError('Unauthorized: Invalid token', '401');
+};
 
-const authValidator = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+const authValidator = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   try {
     if (!isProtectedRoute(req.body.query)) return next();
 
@@ -59,9 +63,9 @@ const authValidator = async (req: Request, res: Response, next: NextFunction): P
     await validateUser(id);
     req.headers.userId = id;
     return next();
-  } catch(error) { 
+  } catch (error) {
     res.status(200).json({ errors: [error] });
   }
-}
+};
 
 export default authValidator;
